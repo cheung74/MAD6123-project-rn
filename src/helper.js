410693416ed75d2752ff43ec6fb30fda72859424
mark chan <mark_chan74@hotmail.com>
2022-11-28 18:21:04 -0500

    create mapper for change task status

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,23 @@
 import moment from "moment";
 
-
 export const getDateString = (date) => {
   return moment(date, "x").format("DD MMM YYYY hh:mm a");
+};
+
+export const updateTaskState = (project, taskId, hour) => {
+  const _project = { ...project };
+  const task = _project.task.find((item) => item.id === taskId);
+  task.hour = hour;
+  task.cost = parseFloat(task.hour) * parseFloat(task.rate).toFixed(2) + "";
+  task.status = "completed";
+  if (
+    Array.isArray(_project.task) &&
+    _project.task.every((item) => item.status === "completed")
+  ) {
+    const total = _project.task.reduce((acm, cur) => {
+      return (acm = acm + parseInt(cur.cost));
+    }, 0);
+    _project.cost = total + "";
+    _project.status = "completed";
+  }
 };
